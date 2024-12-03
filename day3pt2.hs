2bb065976d8ff068fba39a11ccb68e5f574dd3f9
Skye <me@skye.vg>
2024-12-03 14:34:28 +0900

    whoops debug message

--- day3pt2.hs
+++ day3pt2.hs
@@ -10,7 +10,6 @@ main = do
         handle <- openFile "day3.txt" ReadMode
         contents <- hGetContents handle
         let splitted = (concatMap ('\n':) . concatMap (cons . splitOn "do()")) (splitOn "don't()" ("do()" ++ contents))
-        print splitted
         let matches = getAllTextMatches (splitted =~ "mul\\(([0-9]+),([0-9]+)\\)") :: [String]
         let answer = (sum . map (uncurry (*) . (\[a, b] -> (a, b)) . map (read :: String -> Int) . cons . (\a -> getAllTextSubmatches (a =~ "mul\\(([0-9]+),([0-9]+)\\)") :: [String]))) matches
         print answer
