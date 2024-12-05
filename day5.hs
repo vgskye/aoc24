4a9bc93b85d1515ac1f83d5578126506104d30b3
Skye <me@skye.vg>
2024-12-05 14:40:01 +0900

    day 5

---
+++ day5.hs
@@ -0,0 +1,23 @@
+import System.IO
+import Control.Monad
+import Data.List
+import Data.List.Split
+import Data.Maybe
+
+isOkay (Just a) (Just b) = a < b
+isOkay _ Nothing = True
+isOkay Nothing _ = True
+
+dothMatch [] l = True
+dothMatch ([a, b]:xs) l = isOkay (elemIndex a l) (elemIndex b l) && dothMatch xs l
+
+middle l = l !! div (length l - 1) 2
+
+main = do
+        handle <- openFile "day5.txt" ReadMode
+        [rules, queers] <- fmap (splitOn [""] . lines) (hGetContents handle)
+        let parsed = map (map read . splitOn "|") rules :: [[Int]]
+        let gays = map (map read . splitOn ",") queers :: [[Int]]
+        let matches = (sum . map middle . filter (dothMatch parsed)) gays
+        print matches
+        hClose handle
