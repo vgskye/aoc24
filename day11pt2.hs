63e3ffdb819d4bd6e21c313efd322842f1fe5676
Skye <me@skye.vg>
2024-12-11 15:28:26 +0900

    better memoize

--- day11pt2.hs
+++ day11pt2.hs
@@ -3,8 +3,8 @@ import Control.Monad
 import Data.List
 import Math.NumberTheory.Logarithms
 import Debug.Trace
-import Control.Parallel.Strategies (rpar, parMap)
-import Data.Function.Memoize (memoize2)
+import Data.Function.Memoize
+import Control.Parallel.Strategies (parMap, rpar)
 
 blinked a
     | a == 0 = [1]
@@ -13,11 +13,11 @@ blinked a
         in [a `div` (10 ^ log), a `mod` (10 ^ log)]
     | otherwise = [a * 2024]
 
-blinkcount 1 l = (length . concatMap blinked) l
-blinkcount n l = (sum . parMap rpar (mblinkcount (n - 1) . blinked)) l
+blinkcount _ 1 l = (length . concatMap blinked) l
+blinkcount self n l = (sum . parMap rpar (self (n - 1) . blinked)) l
 
 mblinkcount :: Int -> [Integer] -> Int
-mblinkcount = memoize2 blinkcount
+mblinkcount = memoFix2 blinkcount
 
 main = do
         handle <- openFile "day11.txt" ReadMode
