e6ebaf8e3685ffce3196ffd11a54d9a7bf445874
Skye <me@skye.vg>
2024-12-12 16:18:36 +0900

    clean mapper up further

--- day12map.hs
+++ day12map.hs
@@ -1,17 +1,17 @@
 import System.IO
 import Data.List
 import Data.Maybe
+import Text.Printf (hPrintf)
 main = do
         handle <- openFile "day12.txt" ReadMode
+        writeHandle <- openFile "day12.pgm" WriteMode
         origmap <- fmap lines (hGetContents handle)
         let width = (length . head) origmap
         let height = length origmap
         let regions = (nub . sort . concat) origmap
-        putStrLn "P2"
-        print width
-        print height
-        print (length regions - 1)
+        hPrintf writeHandle "P2 %d %d %d " width height (length regions - 1)
         let pgmd = concatMap (mapMaybe (`elemIndex` regions)) origmap
         let stringified = foldr (\v acc -> show v ++ ' ':acc) "" pgmd
-        putStrLn stringified
+        hPutStrLn writeHandle stringified
+        hClose writeHandle
         hClose handle
