adefe6b499823489c342cf94575a96841ee861fc
Skye <me@skye.vg>
2024-12-13 14:33:59 +0900

    linear 13

---
+++ day13.hs
@@ -0,0 +1,23 @@
+import System.IO
+import Data.List
+import Data.List.Split (splitOn)
+import Text.Regex.TDFA
+import GHC.Base (IO(IO))
+
+pat = "Button A: X\\+([0-9]+), Y\\+([0-9]+)\nButton B: X\\+([0-9]+), Y\\+([0-9]+)\nPrize: X=([0-9]+), Y=([0-9]+)"
+
+linalgStuff [xa, ya, xb, yb, xt, yt] =
+    let det = xa * yb - xb * ya in
+    let x = (yb * xt) + (-xb * yt) in
+    let y = (-ya * xt) + (xa * yt) in
+        if x `mod` det /= 0 || y `mod` det /= 0
+        then (0, 0)
+        else (x `div` det, y `div` det)
+
+tokens (a, b) = a * 3 + b
+
+main = do
+        handle <- openFile "day13.txt" ReadMode
+        machines <- fmap (sum . map (tokens . linalgStuff . map read . tail . (\e -> getAllTextSubmatches (e =~ pat))) . splitOn "\n\n") (hGetContents handle)
+        print machines
+        hClose handle
